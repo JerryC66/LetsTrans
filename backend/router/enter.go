48736fc6a334eaeccffb3ee48f0002bd001b22b2
FirwoodLin <tan90_1697@163.com>
2024-05-17 17:42:08 +0800

    feat(Backend): file upload

--- backend/router/enter.go
+++ backend/router/enter.go
@@ -1,11 +1,13 @@
 package router
 
 import (
+	"github.com/firwoodlin/letstrans/router/letstrans"
 	"github.com/firwoodlin/letstrans/router/system"
 )
 
 type RouterGroup struct {
 	System system.RouterGroup
+	File   letstrans.RouterGroup
 }
 
 var RouterGroupApp = new(RouterGroup)
